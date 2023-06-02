a049ab1871a3accfa2b1092316963fbab3d8d850
Adricop98 <adricop98@gmail.com>
2023-06-02 15:06:41 +0200

    versione stabile 3 leaf 2 spine

--- setup-vxlan.sh
+++ setup-vxlan.sh
@@ -13,3 +13,11 @@ docker exec -it clab-vxlan_frr_simple-r3 ip link add vxlan10 type vxlan id 10 ds
 docker exec -it clab-vxlan_frr_simple-r3 ip link set dev vxlan10 up
 docker exec -it clab-vxlan_frr_simple-r3 ip link set vxlan10 master br0
 docker exec -it clab-vxlan_frr_simple-r3 ip link set eth1 master br0
+
+docker exec -it clab-vxlan_frr_simple-r5 ip link add br0 type bridge
+docker exec -it clab-vxlan_frr_simple-r5 ip link set dev br0 up
+docker exec -it clab-vxlan_frr_simple-r5 ip link add vxlan10 type vxlan id 10 dstport 4789
+docker exec -it clab-vxlan_frr_simple-r5 ip link set dev vxlan10 up
+docker exec -it clab-vxlan_frr_simple-r5 ip link set vxlan10 master br0
+docker exec -it clab-vxlan_frr_simple-r5 ip link set eth2 master br0
+
